376c144d4425e66b4c9739c4df8d106e91e0ab28
benboba <benboba@orbbec.com>
2022-02-15 15:57:43 +0800

    123

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"os/exec"
@@ -11,12 +12,19 @@ import (
 var Version = ""
 
 func main() {
-
+	outFile := ""
+	flag.StringVar(&outFile, "o", "", "输入文件，默认为当前包名")
+	flag.Parse()
+	org := make([]string, 0)
 	Version = strconv.FormatInt(time.Now().Unix(), 10)
+	org = append(org, "build", "-ldflags", fmt.Sprintf("-X 'main.Version=%s'", Version))
+	if outFile != "" {
+		org = append(org, "-o", outFile)
+	}
 
 	log.Println(Version)
 
-	cmd := exec.Command("go", "build", "-ldflags", fmt.Sprintf("-X 'main.Version=%s'", Version))
+	cmd := exec.Command("go", org...)
 
 	outPut, err := cmd.Output()
 	if err != nil {
